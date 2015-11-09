427bc080f5e6cee7005e401408fc8c3976ef3198
d023650 <dirk.luedtke@sap.com>
2015-11-09 20:03:17 +0100

    DB Setup Files

---
+++ setup/vereinDBcreate.sql
@@ -0,0 +1,15 @@
+﻿-- Database: verein
+
+-- DROP DATABASE verein;
+
+CREATE DATABASE verein
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'German_Germany.1252'
+       LC_CTYPE = 'German_Germany.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE verein
+  IS 'Verwaltung des Vereins';
+
